ae1f8fe905eedf99dd3e92e6e5863f5b2123d797
itejada <itejadac98@elpuig.xeill.net>
2018-09-21 19:27:25 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner;
+
+        System.out.println("introduce un primer valor");
+        int num1= scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("introduce un segundo valor");
+        int num2= scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Que operacion desea realizar");
+
+        switch (input){
+
+            case +:
+                System.out.println();
+                break;
+
+            case  -:
+                System.out.println();
+                break;
+
+            case  *:
+                System.out.println();
+                break;
+
+            case  /:
+                System.out.println();
+                break;
+
+
+        }
+
+
+
+
+    }
+}
